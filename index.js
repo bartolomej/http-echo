1a12ae706591295c72784a782f254eef93555efc
Bart <bartolomej.kozorog@gmail.com>
2021-09-06 18:17:07 +0200

    feat: advanced req parsing, split logic into multiple functions, history filtering

--- index.js
+++ index.js
@@ -7,54 +7,150 @@ const history = [];
 
 const server = net.createServer(connection => {
 
-  function sendHttpResponse(strData) {
-    connection.write(`HTTP/1.1 200 OK\n\n${strData}`)
-    connection.end();
-  }
-
-  function logHistory(data) {
-    if (history.length > MAX_HISTORY) {
-      history.splice(0, 1);
-    }
-    history.push(data);
-  }
-
   connection.on('data', data => {
-    const strData = data.toString();
+    const req = decodeHttpRequest(data);
+    const res = sendHttpResponse(connection);
+    if (!req) return;
 
-    // log every incoming message to stdout
-    process.stdout.write(strData + '\n\n')
-
-    const httpReq = parseHttpMessage(strData);
-
-    if (httpReq && httpReq.path === '/history') {
-      sendHttpResponse(history.reverse().join("\n\n"));
-    } else {
-      sendHttpResponse(strData);
+    switch (req.endpoint) {
+      case "/history":
+        return onHistoryRequest(req, res);
+      default:
+        onDefaultRequest(req, res);
     }
 
-    // store current request data to history
-    logHistory(strData);
+    onRequest(req, res)
   })
 
-  connection.on('error', error => {
-    process.stderr.write(`[ERROR]: ${error}\n`);
-  })
+  connection.on('error', error => logError(error.message))
 
 })
 
-server.on('error', error => {
-  process.stdout.write(`[ERROR]: ${error.message}`);
-})
+server.on('error', error => logError(error.message))
 
 server.listen(PORT);
 
-function parseHttpMessage(strData) {
+logInfo(`tcp-echo server started on port ${PORT}`);
+
+
+// REQUEST HANDLERS
+
+function onHistoryRequest (req, res) {
+  const searchQuery = req.query.search;
+  const body = history
+    .filter(e => searchQuery ? new RegExp(searchQuery).test(e.rawData) : true);
+  if (acceptsJson(req)) {
+    res({ json: body })
+  } else {
+    res({ text: body.map(e => e.rawData).join("\n\n\n") })
+  }
+}
+
+function onDefaultRequest (req, res) {
+  if (acceptsJson(req)) {
+    res({ json: req })
+  } else {
+    res({ text: req.rawData })
+  }
+  // store current request data to history
+  logHistory(req);
+}
+
+function onRequest (req, res) {
+  // log every incoming message to stdout
+  process.stdout.write(req.rawData + '\n\n')
+}
+
+// UTILITY FUNCTIONS
+
+function logHistory (data) {
+  if (history.length > MAX_HISTORY) {
+    history.splice(0, 1);
+  }
+  history.push(data);
+}
+
+function sendHttpResponse (connection) {
+  return function (options) {
+    connection.write(encodeHttpRequest(options))
+    connection.end();
+  }
+}
+
+function encodeHttpRequest ({ text = '', json, code = 200, headers = {} }) {
+  const headersStr = Object.keys(headers)
+    .map(e => `${e}: ${headers[e]}`)
+    .join("\n");
+  const body = json ? JSON.stringify(json) : text;
+  return `HTTP/1.1 ${code} OK` +
+    (headersStr.length > 0 ? `\n${headersStr}` : '') +
+    "\n\n" +
+    body;
+}
+
+function decodeHttpRequest (data) {
+  const strData = data.toString();
   const httpMatch = strData.match(/ HTTP/);
   if (!httpMatch) {
+    // not a valid HTTP request
     return null;
   }
-  const req = strData.substring(0, httpMatch.index);
-  const [method, path] = req.split(" ");
-  return {method, path};
+
+  const bodySeparatorIndex = strData.indexOf("\n\n");
+  const [initialLine, ...headerLines] = strData
+    // take whole string if body separator isn't found (there is no body)
+    .substring(0, bodySeparatorIndex < 0 ? strData.length : bodySeparatorIndex)
+    .split("\n");
+  const [method, path, protocol] = initialLine.split(" ");
+  const headers = headerLines
+    .map(e => {
+      const [key, value] = e.split(": ");
+      if (!(key && value)) return null;
+      return { key, value: parseKeyValue(value) };
+    })
+    .filter(e => !!e)
+    .reduce((p, c) => ({ ...p, [c.key]: c.value }), {});
+  const [endpoint, queryStr] = path.split("?")
+  const query = queryStr
+    ? queryStr
+      .split("&")
+      .filter(e => !!e) // include only non-empty values
+      .map(e => {
+        const [key, value] = e.split("=");
+        return { key, value: parseKeyValue(value) }
+      })
+      .reduce((p, c) => ({ ...p, [c.key]: c.value }), {})
+    : {};
+  return { method, path, endpoint, query, protocol, headers, rawData: strData };
+}
+
+function parseKeyValue(value) {
+  const strValue = value.trim();
+  const numberValue = parseFloat(strValue);
+  const isNumber = !Number.isNaN(numberValue);
+  const parsedStrValue = strValue.includes(",")
+    ? strValue.split(",").map(e => e.trim()).filter(e => !!e)
+    : strValue;
+  return isNumber ? numberValue : parsedStrValue;
+}
+
+function acceptsJson (req) {
+  // may have multiple values seperated by comma
+  // text/html,application/xhtml+xml,application/xml
+  console.log(req.headers["Accept"]);
+  return req.headers["Accept"].findIndex(e => equalsIgnoreCase(e, "application/json"))
+}
+
+function equalsIgnoreCase (a, b) {
+  return a.toLowerCase() === b.toLowerCase();
+}
+
+// LOGGER FUNCTIONS
+
+function logInfo(message) {
+  process.stdout.write(`[INFO]: ${message}\n`);
+}
+
+function logError(message) {
+  process.stderr.write(`[ERROR]: ${message}\n`);
 }
